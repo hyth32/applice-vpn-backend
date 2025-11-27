6557ccae6ddffa63ccdc5210886a0ea681d25ed5
Ivan Goryaev <5474n.sage@gmail.com>
2025-11-27 18:56:10 +0300

    добавил глобальную типизацию реквеста после валидации

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,17 +1,19 @@
 import { NextFunction, Request, Response } from 'express'
 import { z } from 'zod'
 
-type AnySchema = z.ZodType
+export type ValidatedRequest<T extends z.ZodTypeAny> = Request<unknown, unknown, z.infer<T>>
 
-export const validate = (schema: AnySchema) => (req: Request, res: Response, next: NextFunction) => {
-  const result = schema.safeParse(req.body)
+export const validate =
+  <T extends z.ZodTypeAny>(schema: T) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body)
 
-  if (!result.success) {
-    const messages = result.error.issues.map((err) => err.message)
-    res.status(400).json({ error: messages.join(', ') })
-    return
+    if (!result.success) {
+      const messages = result.error.issues.map((err) => err.message)
+      res.status(400).json({ error: messages.join(', ') })
+      return
+    }
+
+    ;(req as ValidatedRequest<T>).body = result.data
+    next()
   }
-
-  req.body = result.data
-  next()
-}
